9d31e3cf6287e9dfef4855587c946fa2bf20b7b3
Sharad <sharad.gd3912@gmail.com>
2020-06-29 11:40:21 +0530

    Initial commit

---
+++ Demo Test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Demo Test
+//
+//  Created by Sharad on 29/06/20.
+//  Copyright © 2020 Sharad. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
